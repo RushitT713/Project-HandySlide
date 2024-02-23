8e6391277c34df15d0167a2925490a7dabddf3fe
Rushit Trambadia <97343333+RushitT713@users.noreply.github.com>
2024-02-23 19:19:59 +0530

    Update powerpoint_control.py

--- HandySlide Project - Codes/powerpoint_control.py
+++ HandySlide Project - Codes/powerpoint_control.py
@@ -1,8 +1,23 @@
 # Python Code - MAIN
+import os
+import sys
+import time
+import serial.tools.list_ports
 import serial
 import pyautogui
 
-ser = serial.Serial('COM10', 9600)
+ports = serial.tools.list_ports.comports(include_links=False)
+for port in ports :
+    print('Find port '+ port.device)
+
+ser = serial.Serial(port.device)
+if ser.isOpen():
+    ser.close()
+
+ser = serial.Serial(port.device, 9600, timeout=1)
+ser.flushInput()
+ser.flushOutput()
+print('Connect ' + ser.name)
 
 while True:
     line = ser.readline().decode('utf-8').strip()
